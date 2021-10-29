dfe218944a4585abd232bf7f1e7ccc7e47155860
talithaRM <93226697+talithaRM@users.noreply.github.com>
2021-10-30 06:28:08 +0800

    Update zeeoneofc.js

--- zeeone/zeeoneofc.js
+++ zeeone/zeeoneofc.js
@@ -110,7 +110,7 @@ ${petik}└❒ ${prefix}anime${petik} _random_
 `
 }
 const allmenu = (prefix, wita, wit, ucapannya2, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensiii, tekss)  => {
-	return`❏───「 *ALPHA BOT* 」───❏
+	return`❏───「 *SOI BOT* 」───❏
      
 ┌❏ *INDONESIA TIME*
 ${petik}│◦➛ WIB : ${timuu}
@@ -655,7 +655,7 @@ ${petik}│◦ ${prefix}rikagusriani
 │
 └─❑「 MENU BOT 」
 
-❏───「 *ALPHA BOT* 」───❏
+❏───「 *SOI BOT* 」───❏
 `
 }
 
